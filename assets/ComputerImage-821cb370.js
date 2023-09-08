a1d3d032cc5d0b2d36981170d089602222bcffb9
Anshuman Parmar <anshumansp16@gmail.com>
2023-09-08 14:20:14 +0530

    Added More Projects

---
+++ assets/ComputerImage-821cb370.js
@@ -0,0 +1,1 @@
+import{j as e,ar as a}from"./index-b1c07e12.js";function s(){return e.jsx("div",{className:"flex justify-center relative top-[45%]",children:e.jsx("img",{className:"h-[250px] w-auto",src:a.img,alt:"computer"})})}export{s as default};
