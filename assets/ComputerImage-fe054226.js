107f79bb49ec4b60f137f9372488914ac681da10
Anshuman Parmar <anshumansp16@gmail.com>
2023-09-08 09:09:07 +0530

    Added image for the mobile using context API

---
+++ assets/ComputerImage-fe054226.js
@@ -0,0 +1,1 @@
+import{j as e,aN as a}from"./index-670e61a2.js";function s(){return e.jsx("div",{className:"flex justify-center relative top-1/2",children:e.jsx("img",{className:"h-[300px] w-auto",src:a.img,alt:"computer"})})}export{s as default};
