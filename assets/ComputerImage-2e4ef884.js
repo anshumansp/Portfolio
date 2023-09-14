a35a6838d4839853bc5a90dd6d6971a12459dfa7
Anshuman Parmar <anshumansp16@gmail.com>
2023-09-14 16:17:35 +0530

    Added Notex

---
+++ assets/ComputerImage-2e4ef884.js
@@ -0,0 +1,1 @@
+import{j as e,g as s}from"./index-3dbd988f.js";function t(){return e.jsx("div",{className:"flex justify-center relative top-[45%]",children:e.jsx("img",{className:"h-[250px] w-auto",src:s.img,alt:"computer"})})}export{t as default};
